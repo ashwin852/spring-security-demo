d74f45943aa8d493358ff68fbae95e987e417b76
ashwin852 <rmaswin0311@gmail.com>
2024-02-16 15:20:08 +0530

    signin api created

---
+++ src/main/java/com/example/securitydemo/controllers/AuthController.java
@@ -0,0 +1,81 @@
+package com.example.securitydemo.controllers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.securitydemo.payload.request.LoginRequest;
+import com.example.securitydemo.payload.response.JwtResponse;
+import com.example.securitydemo.payload.response.MessageResponse;
+import com.example.securitydemo.repository.RoleRepository;
+import com.example.securitydemo.repository.UserRepository;
+import com.example.securitydemo.security.jwt.JwtUtils;
+import com.example.securitydemo.security.services.UserDetailsImpl;
+import jakarta.validation.Valid;
+
+
+
+/*allows requests from any origin to access the annotated controller method or class, 
+ * and it specifies that the preflight request response can be cached for up to 1 hour
+ *  by the client's browser. 
+*/
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+	
+	@Autowired
+	AuthenticationManager authenticationManager;
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	RoleRepository roleRepository;
+	
+	@Autowired
+	PasswordEncoder encoder;
+	
+	@Autowired
+	JwtUtils jwtUtils;
+	
+	@PostMapping("/signin")
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+		
+		/*
+		 * Plays a central role in username/password-based authentication in Spring
+		 * Security, allowing applications to securely authenticate users using their
+		 * credentials
+		 */		
+		Authentication authentication = authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+		
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
+		String jwt = jwtUtils.generateJwtToken(authentication);
+		
+		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+		List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
+				.collect(Collectors.toList());
+		
+		return ResponseEntity 
+				.ok(new JwtResponse(jwt, userDetails.getId(), userDetails.getUsername(), userDetails.getEmail(), roles));
+	}
+	
+	@PostMapping("/signup")
+	public ResponseEntity<?> registerUser() {
+		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	}
+	
+}
